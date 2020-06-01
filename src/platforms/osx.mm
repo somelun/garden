d99295946408a090a102adf349ce31efcfe1ef3a
somelun <info@lunatic.su>
2020-06-01 18:18:38 +0300

    Add menu bar for osx, add Quit button for menubar

--- src/platforms/osx.mm
+++ src/platforms/osx.mm
@@ -66,7 +66,18 @@ Application::Application() {
         [NSApplication sharedApplication];
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 
-        //
+        id menubar = [[NSMenu new] autorelease];
+        id appMenuItem = [[NSMenuItem new] autorelease];
+        [menubar addItem:appMenuItem];
+        [NSApp setMainMenu:menubar];
+
+        id appMenu = [[NSMenu new] autorelease];
+        id appName = [[NSProcessInfo processInfo] processName];
+        id quitTitle = [@"Quit " stringByAppendingString:appName];
+        id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
+                                                      action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
+        [appMenu addItem:quitMenuItem];
+        [appMenuItem setSubmenu:appMenu];
 
         [NSApp finishLaunching];
     }
