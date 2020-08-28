af175656bd6b9f765ead47eac2f46239f48972e7
somelun <info@lunatic.su>
2020-08-28 17:54:31 +0300

    Small refactoring

--- src/engine/scenes/base_scene.h
+++ src/engine/scenes/base_scene.h
@@ -3,7 +3,6 @@
 #include "../framebuffer.h"
 #include <algorithm>
 
-#include <iostream>
 
 /**
  * This class should not be used at all.
@@ -21,9 +20,6 @@ protected:
         uint8_t* data = buffer.get_data();
 
         uint32_t index = (x * buffer.get_width() + y) * 4;
-
-        // std::cout << x << ", " << y << ", " << index << std::endl;
-
         for (int i = 0; i < 4; ++i) {
             data[index + i] = 0;
         }
@@ -37,7 +33,7 @@ protected:
                             uint16_t y1) {
 
         bool steep = false;
-        if (std::abs(x0-x1) < std::abs(y0-y1)) {
+        if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
             std::swap(x0, y0);
             std::swap(x1, y1);
             steep = true;
@@ -46,12 +42,12 @@ protected:
             std::swap(x0, x1);
             std::swap(y0, y1);
         }
-        int dx = x1-x0;
-        int dy = y1-y0;
-        int derror2 = std::abs(dy)*2;
-        int error2 = 0;
-        int y = y0;
-        for (int x=x0; x<=x1; x++) {
+        int16_t dx = x1 - x0;
+        int16_t dy = y1 - y0;
+        uint16_t derror2 = std::abs(dy) * 2;
+        uint16_t error2 = 0;
+        uint16_t y = y0;
+        for (size_t x = x0; x <= x1; ++x) {
             if (steep) {
                 draw_pixel(buffer, y, x);
             } else {
@@ -59,8 +55,8 @@ protected:
             }
             error2 += derror2;
             if (error2 > dx) {
-                y += (y1>y0?1:-1);
-                error2 -= dx*2;
+                y += (y1 > y0 ? 1: -1);
+                error2 -= dx * 2;
             }
         }
     }
