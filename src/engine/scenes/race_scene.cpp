745172fbc3d3ca6ed46df7d1e74666cbf25ad248
somelun <info@lunatic.su>
2020-09-06 13:12:07 +0300

    Moving terrain

--- src/engine/scenes/race_scene.cpp
+++ src/engine/scenes/race_scene.cpp
@@ -1,15 +1,20 @@
 #include "race_scene.h"
 #include "../render2d.h"
 
+#include <math.h>
+
 #include <iostream>
 
 
 // TODO: move all constans from here later
-const color_t WHITE  = {255, 255, 255, 255};
-const color_t GREEN  = {  0, 255,   0, 255};
-const color_t RED    = {255,   0,   0, 255};
-const color_t BLUE   = {  0,   0, 255, 255};
-const color_t GREY   = {128, 128, 128, 255};
+const color_t WHITE        = {255, 255, 255, 255};
+const color_t GREEN        = {  0, 255,   0, 255};
+const color_t DARK_GREEN   = {0, 101, 51, 255};
+const color_t RED          = {255,   0,   0, 255};
+const color_t BLUE         = {  0,   0, 255, 255};
+const color_t GREY         = {128, 128, 128, 255};
+
+static float distance = 0.0f;
 
 
 void RaceScene::update_buffer(Framebuffer& buffer) {
@@ -18,7 +23,7 @@ void RaceScene::update_buffer(Framebuffer& buffer) {
     uint16_t width = buffer.get_width();
     uint16_t height = buffer.get_height();
 
-    // std::cout << "======================================" << std::endl;
+    distance += 10.0f * 0.1f;
 
     for (size_t y = height / 2; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
@@ -26,26 +31,18 @@ void RaceScene::update_buffer(Framebuffer& buffer) {
             float perspective = (float)(y - height / 2) / height;
 
             float middle_point = 0.5f;
-            float road_width = perspective;
+            float road_width = 0.05f + perspective * 0.85f;
             float clip_width = road_width * 0.15f;
-
-            // road_width *= 0.5f;
 
             int left_grass = (middle_point - road_width - clip_width) * width;
             int left_clip = (middle_point - road_width) * width;
             size_t right_clip = (middle_point + road_width) * width;
             size_t right_grass = (middle_point + road_width + clip_width) * width;
 
-            // if (y < 440) {
-            //     continue;
-            // }
-
-            // std::cout << y << std::endl;
-
-            // std::cout << left_grass << ", " << x << ", " << left_clip << std::endl;
+            color_t grass_color = sinf(20.0f * powf(1.0f - perspective, 3) + distance * 0.1f) > 0.0f ? GREEN : DARK_GREEN;
 
             if (x >= 0 && x < left_grass) {
-                draw_pixel(buffer, x, y, GREEN);
+                draw_pixel(buffer, x, y, grass_color);
             } else
             if (x >= left_grass && x < left_clip) {
                 draw_pixel(buffer, x, y, RED);
@@ -60,11 +57,9 @@ void RaceScene::update_buffer(Framebuffer& buffer) {
             }
             else
             if (x >= right_grass && x < width) {
-                draw_pixel(buffer, x, y, GREEN);
+                draw_pixel(buffer, x, y, grass_color);
             }
         }
     }
     // abort();
-//
-    // std::cout << "======================================" << std::endl;
 }
