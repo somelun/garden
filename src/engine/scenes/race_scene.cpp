eedf9281bd596a3f50d5447a34acf8d9a7bc5b2a
somelun <info@lunatic.su>
2020-09-19 21:50:31 +0300

    Second attempt tp make curves

--- src/engine/scenes/race_scene.cpp
+++ src/engine/scenes/race_scene.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 const uint16_t kRoadHeight = 300;
+const uint16_t kSpeed = 100;
 
 
 // TODO: move all constans from here later
@@ -22,6 +23,17 @@ RaceScene::RaceScene(Framebuffer& buffer) : buffer_(buffer) {
 
     width_ = buffer_.get_width();
     height_ = buffer_.get_height();
+
+    uint16_t fov_angle = 60;
+    float y_world = sin(fov_angle / 2);
+    float z_world = (fov_angle / 2);
+
+    // x_screen = (y_world*scaling)/z_world + (x_resolution/2)
+    // 640 = (sin(30)*scaling/cos(30)) + 320
+    // 320 = tan(30)*scaling
+    // 320/tan(30) = scaling
+
+    float scaling = (width_ / 2.0f) / tan(fov_angle / 2.0f);
 
     track_data_.push_back(std::make_pair(0.0f, 10.0f));
     track_data_.push_back(std::make_pair(0.2f, 200.0f));
@@ -41,15 +53,47 @@ RaceScene::RaceScene(Framebuffer& buffer) : buffer_(buffer) {
 
 void RaceScene::update(double dt) {
     // update2(dt);
-
     // return;
 
+    distance_ += kSpeed * dt;
+
     size_t initial_y = height_ - kRoadHeight;
-    for (size_t y = initial_y; y < height_; ++y) {
+
+    for (size_t y = height_ / 2; y < height_; ++y) {
         for (int x = 0; x < width_; ++x) {
 
-            draw_pixel(buffer_, x, y, DARK_GREEN);
+            float perspective = (float)(y - height_ / 2) / height_;
 
+            float middle_point = 0.5f;
+
+            float road_width = 0.05f + perspective * 0.85f;
+            float clip_width = road_width * 0.15f;
+
+            int left_grass = (middle_point - road_width - clip_width) * width_;
+            int left_clip = (middle_point - road_width) * width_;
+            int right_clip = (middle_point + road_width) * width_;
+            int right_grass = (middle_point + road_width + clip_width) * width_;
+
+            if (x >= 0 && x < left_grass) {
+                draw_pixel(buffer_, x, y, DARK_GREEN);
+            } else
+            if (x >= left_grass && x < left_clip) {
+                draw_pixel(buffer_, x, y, RED);
+            }
+            else
+            if (x >= left_clip && x < right_clip) {
+                draw_pixel(buffer_, x, y, GREY);
+            }
+            else
+            if (x >= right_clip && x <= right_grass) {
+                draw_pixel(buffer_, x, y, RED);
+            }
+            else
+            if (x >= right_grass && x < width_) {
+                draw_pixel(buffer_, x, y, DARK_GREEN);
+            }
+
+            // draw_pixel(buffer_, x, y, DARK_GREEN);
         }
     }
 }
