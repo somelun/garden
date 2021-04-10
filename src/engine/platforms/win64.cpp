200ead3cda679e7adb92e984975cac44a0d59a3f
somelun <info@lunatic.su>
2021-04-10 10:51:38 +0100

    Win64 support (work in progress)

--- src/engine/platforms/win64.cpp
+++ src/engine/platforms/win64.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include <iostream>
 
-WNDCLASS window_class = {};
+WNDCLASS WindowClass = {};
 
 // Win64 window implementation
 struct window_impl_t {
@@ -12,88 +12,99 @@ struct window_impl_t {
     bool bClosing{ false };
 };
 
-static LRESULT CALLBACK main_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
+static LRESULT CALLBACK MainCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hDc = BeginPaint(window, &ps);
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
 
-        FillRect(hDc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            HDC hDc = BeginPaint(window, &ps);
+            FillRect(hDc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(window, &ps);
 
-        EndPaint(window, &ps);
+            return 0;
+        }
 
-        return 0;
+        case WM_CLOSE: {
+            return 0;
+        }
+
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+
+            return 0;
+        }
     }
-    case WM_DESTROY:
-    {
-        PostQuitMessage(0);
-        return 0;
-    }
-    }
 
-    return DefWindowProc(window, message, w_param, l_param);
+    return DefWindowProc(window, message, wParam, lParam);
 }
 
-Application::Application() {
+Application::Application(const char* title, uint16_t width, uint16_t height) {
     std::cout << "Application launching...\n";
-    window_class = {};
 
-    window_class.style = CS_HREDRAW|CS_VREDRAW;
-    window_class.lpfnWndProc = main_callback;
-    window_class.cbClsExtra = 0;
-    window_class.cbWndExtra = 0;
-    window_class.hInstance = GetModuleHandle(nullptr);
-    window_class.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    window_class.hCursor = LoadCursor(NULL, IDC_ARROW);
-    window_class.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    window_class.lpszMenuName = NULL;
-    window_class.lpszClassName = "sandbox";
+    WindowClass = {};
+    WindowClass.style = CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainCallback;
+    WindowClass.cbClsExtra = 0;
+    WindowClass.cbWndExtra = 0;
+    WindowClass.hInstance = GetModuleHandle(nullptr);
+    // WindowClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    // WindowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WindowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    WindowClass.lpszMenuName = NULL;
+    WindowClass.lpszClassName = title;
 
-    RegisterClass(&window_class); 
+    if (RegisterClass(&WindowClass)) {
+        CreateWindow(title, width, height);
+    }
 }
 
 Application::~Application() {
     std::cout << "Application closing...\n";
 }
 
-void Application::create_window(const char* title, uint16_t width, uint16_t height) {
+void Application::CreateWindow(const char* title, uint16_t width, uint16_t height) {
     if (window_impl == nullptr) {
         window_impl = new window_impl_t();
     }
- 
+
     // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
     window_impl->handler = CreateWindowEx(
         0,
-        window_class.lpszClassName,
-        "HandmadeHero",
+        WindowClass.lpszClassName,
+        title,
         WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        CW_USEDEFAULT, // x
+        CW_USEDEFAULT, // y
         width,
         height,
-        0,
-        0,
-        GetModuleHandle(nullptr),
+        0, // parent window
+        0, // menu
+        GetModuleHandle(nullptr), // instance
         0
     );
 
-    ShowWindow(window_impl->handler, SW_SHOW);
+    if (window_impl->handler) {
+        ShowWindow(window_impl->handler, SW_SHOW);
+    }
 }
 
-void Application::close_window() {
+void Application::CloseWindow() {
     delete window_impl;
 }
 
-void Application::draw_buffer(class Framebuffer* buffer) {
+void Application::DrawBuffer(const class Framebuffer* buffer) {
     //
 }
 
-void Application::handle_event() {
-    //
+void Application::HandleEvent() {
+    MSG message;
+    while (PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
 }
 
-bool Application::is_running() {
+bool Application::IsRunning() {
     return !window_impl->bClosing;
 }
 
